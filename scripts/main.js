c47e43ae12e6d6a0d1baa6b1a38dca4028a40f55
LeowWB <41581512+LeowWB@users.noreply.github.com>
2019-01-19 16:01:29 +0800

    improved js structure, added basic comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 let submit_button = document.getElementById("submit");
 let upload_control = document.getElementById("upload");
 
+//event handler for when submit button is clicked
 function processUpload() {
 
 	let image_file = upload_control.files[0];
@@ -10,11 +11,21 @@ function processUpload() {
 	console.log(product_name);
 }
 
+//given an image file, obtains the text contained within the image
 function getImageText(img) {
 
 }
 
+//obtains all existing text mappings from the db
+function getAllMappings() {
+
+}
+
+//given the text obtained from an image, attempts to map it to a specific product
 function mapTextToProduct(txt) {
+	
+	let mappings = getAllMappings();
+
 	
 }
 
